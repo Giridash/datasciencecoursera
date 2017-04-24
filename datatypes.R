681572c188c6520b5cc1ec843252cfb9f85ea9db
giridash <giridharjr@yahoo.com>
2017-04-23 20:40:44 -0400

    Third commit

--- datatypes.R
+++ datatypes.R
@@ -8,3 +8,22 @@ z <- 1:20
 z
 ## comment
 ## comment 2
+g <- c(0.5,0.7)
+g
+h <- vector("numeric",length=10)
+h
+y <- c(TRUE,2)
+y
+y <- c("c",2)
+y
+y <- c(TRUE,"a")
+y
+x <- 0:6
+class(x)
+as.numeric(x)
+as.logical(x)
+as.character(x)
+x <- c("a","b","c")
+as.numeric(x)
+x <- list("a",1,2,TRUE)
+x
