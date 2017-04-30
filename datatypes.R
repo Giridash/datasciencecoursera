0844edb3f0d3dcdd2017e1363bb5455f53e7de0b
giridash <giridharjr@yahoo.com>
2017-04-30 13:31:36 -0400

    Fourth commit

--- datatypes.R
+++ datatypes.R
@@ -76,4 +76,103 @@ x <- matrix(1:4,nrow = 2, ncol = 2)
 dimnames(x) <- list(c("a","b"),c("d","e"))
 x
 
+## reading tabular data
+data <- read.table("foo.txt")
+
+## reading large data set
+initialize <- read.table("datatable.txt",nrows = 100 )
+classes <- sappy(inital,class)
+tabAll <- read.table("datatable.txt",colClasses = classes )
+
+## textual data Formats
+y <- data.frame(a = 1, b = "a")
+dput(y)
+dput(y,file = "y.R")
+new.y <- dget("y.R")
+new.y
+
+## dump
+x <- "foo"
+y <- data.frame(a = 1, b = "a")
+dump(c("x","y"),file = "data.R")
+rm(x,y)
+source("data.R")
+x
+y
+
+## interfaces to the outside world
+## file, gzfile, bzfile, url
+
+data <- read.csv("file1.csv")
+
+con <- gzfile ("words.gz")
+x <- readLines(data,10)
+x
+
+## readlines from Webpage
+con <- url("http://www.google.com","r")
+x <- readLines(con)
+head(x)
+
+## Subsetting R 
+x <- c(1,2,3)
+x[2]
+x[1:2]
+x[x <2]
+u <- x >1
+u
+
+## list subsetting
+x <- list(foo = 1:4,bar = 0.6, baz = "hello")
+x
+x[1]
+x[[1]]
+
+x$bar
+x[["bar"]]
+x["bar"]
+
+x[c(1,3)]
+
+## subsetting a matrix
+x <- matrix(1:6,2,3)
+x[1,2]
+x[1,]
+## get in matrix fashion
+x[1,2,drop=FALSE]
+
+## partial matching
+x <-list(aartwork=  1:5)
+x$a
+x[["a"]]
+x[["a",exact = FALSE]]
+
+## remiving NA values
+x <- c(1,2,3,NA,5,NA)
+bad <- is.na(x)
+bad
+x[!bad]
+
+## removing NA values
+x <- c(1,2,3,NA,5,NA)
+y <- c("a",NA,"c",NA,"d",NA)
+good <- complete.cases(x,y)
+good
+x[good]
+y[good]
+
+## vectorized operations
+x <- c(1,2,3)
+y <- c(4,5,6)
+x + y
+x > 2
+
+
+x <- matrix(1:4,2,2)
+y <- matrix(4:8,2,2)
+x*y
+
+## true matrix multipliccaiton
+x %% y
+
 
